c83a4273a04edda3537587e55808e1ef52a09abb
Anubhav Srivastava <asrivastava@yugabyte.com>
2023-09-12 21:35:57 -0400

    fix build

--- src/static_vars.cc
+++ src/static_vars.cc
@@ -76,7 +76,6 @@ PageHeapAllocator<StackTrace> Static::stacktrace_allocator_;
 Span Static::sampled_objects_;
 StackTrace* Static::growth_stacks_ = NULL;
 Static::PageHeapStorage Static::pageheap_;
-std::atomic<int64_t> Static::sample_period_;
 
 void Static::InitStaticVars() {
   sizemap_.Init();
@@ -105,8 +104,6 @@ void Static::InitStaticVars() {
                          kDefaultAggressiveDecommit);
 
   pageheap()->SetAggressiveDecommit(aggressive_decommit);
-
-  sample_period_ = 0;
 
   inited_ = true;
 
